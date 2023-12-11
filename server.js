102514deb0e67d4332099210f3b679abd8c2f221
s3877629@rmit.edu.vn <73870711+mookee2809@users.noreply.github.com>
2023-12-11 12:33:46 +0700

    Add files via upload

--- server.js
+++ server.js
@@ -1,34 +1,40 @@
+// variacble for express. js
 const express = require('express');
-
+const sqlite3 = require('sqlite3').verbose();
+const path = require('path');
 
 const app = express();
+const dbPath = path.resolve(__dirname, 'dictionary.db');
+const db = new sqlite3.Database(dbPath, sqlite3.OPEN_READONLY, (err) => {
+  if (err) {
+    console.error(err.message);
+  } else {
+    console.log('Connected to the dictionary database.');
+  }
+});
 
-// Define the route that proxies to the dictionary API
-app.get('/search/:word', async (req, res) => {
-    const url = `https://api.dictionaryapi.dev/api/v2/entries/en/${req.params.word}`;
-
-    try {
-        const response = await fetch(url);
-        if (response.ok) {
-            const data = await response.json();
-            res.json(data);
-        } else {
-            // send error like cannot get if the api does not connect
-            res.status(response.status).json({ message: response.statusText });
-        }
-    } catch (error) {
-        // Cannot /Get
-        res.status(500).json({ message: error.message });
+// Define the search endpoint that queries your SQLite database
+app.get('/search/:word', (req, res) => {
+  const sql = 'SELECT lemma, synonyms, antonyms, definition FROM words WHERE lemma = ?';
+  db.get(sql, [req.params.word], (err, row) => {
+    if (err) {
+      res.status(500).json({ error: err.message });
+      return;
     }
+    if (row) {
+      res.json(row);
+    } else {
+      res.status(404).json({ message: 'Word not found' });
+    }
+  });
 });
 
-// if every check goes fine then print runnning
+// Optionally, you can define a root route just to confirm that the server is running
 app.get('/', (req, res) => {
-    res.send('Dictionary API is running.');
+  res.send('Dictionary Database is connected.');
 });
 
-//port number
 const port = process.env.PORT || 5000;
 app.listen(port, () => {
-    console.log(`Server listening on port ${port}`);
+  console.log(`Server running on port ${port}`);
 });
