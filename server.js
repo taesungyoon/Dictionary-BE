67184c040ec3a3372c77c16822d1b3a41f67a206
s3877629@rmit.edu.vn <73870711+mookee2809@users.noreply.github.com>
2023-12-14 17:44:46 +0700

    Add files via upload

--- server.js
+++ server.js
@@ -1,19 +1,24 @@
-// variacble for express. js
+// variable for express. js
 const express = require('express');
 const sqlite3 = require('sqlite3').verbose();
 const path = require('path');
-
+const cors = require('cors');
 const app = express();
+
+
+// enable cors to all routing
+app.use(cors());
+
 const dbPath = path.resolve(__dirname, 'dictionary.db');
 const db = new sqlite3.Database(dbPath, sqlite3.OPEN_READONLY, (err) => {
   if (err) {
-    console.error(err.message);
+    console.error('Error connecting to the database:', err.message);
   } else {
     console.log('Connected to the dictionary database.');
   }
 });
 
-// Define the search endpoint that queries your SQLite database
+// Search end point for the database
 app.get('/search/:word', (req, res) => {
   const sql = 'SELECT lemma, synonyms, antonyms, definition FROM words WHERE lemma = ?';
   db.get(sql, [req.params.word], (err, row) => {
@@ -29,7 +34,20 @@ app.get('/search/:word', (req, res) => {
   });
 });
 
-// Optionally, you can define a root route just to confirm that the server is running
+//Autocomplete endpoint with database
+app.get('/autocomplete/:partialWord', (req, res) => {
+  const partialWord = req.params.partialWord;
+  const sql = 'SELECT lemma FROM words WHERE lemma LIKE ? LIMIT 10';
+  db.all(sql, [`${partialWord}%`], (err, rows) => {
+    if (err) {
+      res.status(500).json({ error: err.message });
+      return;
+    }
+    const suggestions = rows.map(row => row.lemma);
+    res.json(suggestions);
+  });
+});
+
 app.get('/', (req, res) => {
   res.send('Dictionary Database is connected.');
 });
