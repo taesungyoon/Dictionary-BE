8b4fdc4f88b907a810d1a697041ce00aef964a1c
s3877629@rmit.edu.vn <73870711+mookee2809@users.noreply.github.com>
2023-12-17 23:49:47 +0700

    Update server.js

--- server.js
+++ server.js
@@ -74,5 +74,5 @@ app.get('/', (req, res) => {
 
 const port = process.env.PORT || 5000;
 app.listen(port, () => {
-  console.log(Server running on port ${port});
+  console.log(`Server running on port ${port}`);
 });
